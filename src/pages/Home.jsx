acf07a55e96f08c3aa9a133a2310c662394facd7
Juan Roldán <juan.roldan.zamudio@gmail.com>
2025-02-13 23:40:35 -0600

    Add Home page layout and styles; configure routing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+// Imports
+import HomeStyles from "../styles/Home.module.css";
+
+const Home = () => {
+	return (
+		<main className={HomeStyles.mainContainer}>
+			<h1>Home</h1>
+		</main>
+	);
+};
+
+export default Home;
