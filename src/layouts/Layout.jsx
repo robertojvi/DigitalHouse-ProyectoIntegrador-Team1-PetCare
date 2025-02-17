fe9138900b273eda7515f6225b82273f9f5daf60
Juan Roldán <juan.roldan.zamudio@gmail.com>
2025-02-16 21:57:42 -0600

    feat: Header implementation
    feat: implement header component with responsive menu and authentication buttons; update styles and remove unused files

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -3,14 +3,14 @@
 import { Outlet } from "react-router-dom";
 
 // Componentes
-// import Navbar from "../Components/Header";
+import Header from "../components/Header";
 // import Footer from "../Components/Footer";
 
 const Layout = () => {
 	return (
 		<div className="">
 			{/* Renderiza tanto el(los) componente(s) (Navbar y Footer) como la ruta (Oulet) */}
-			{/* <Header /> */}
+			<Header />
 			<Outlet />
 			{/* <Footer /> */}
 		</div>
