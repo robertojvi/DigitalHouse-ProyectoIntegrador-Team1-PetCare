d831788d8069a74b9ac033ec80a1dc3b3791a454
Juan Roldán <juan.roldan.zamudio@gmail.com>
2025-02-19 11:27:28 -0600

    feat: 404 page
    feat: Enhance 404 error page styling

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,15 @@ function App() {
 				{/* <Route path="/contact" element={<Contact />} />
 				<Route path="/dentist/:id" element={<Detail />} />
 				<Route path="/favs" element={<Favs />} /> */}
-				<Route path="*" element={<h1>Page not found - Error 404</h1>} />
+				<Route
+					path="*"
+					element={
+						// <h1 style="padding-top: 30vh;">
+						<h1 className="mainContainer">
+							Page not found - Error 404
+						</h1>
+					}
+				/>
 			</Route>
 		</Routes>
 		// </>
