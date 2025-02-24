08bda3a144c53c6b12d7b20ddeeda846a1cdeb81
luciacerpap <luciacerpap@gmail.com>
2025-02-24 14:28:38 -0600

    mensajes al logear o registrar

--- src/auth/AuthContext.jsx
+++ src/auth/AuthContext.jsx
@@ -1,18 +1,20 @@
 import React, { createContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
   const [auth, setAuth] = useState({
     token: localStorage.getItem("token"),
     role: localStorage.getItem("role"),
-    nombre: localStorage.getItem("userName"),
+    nombre: localStorage.getItem("userName")
   });
-
+  
   const logout = () => {
     localStorage.clear();
     setAuth({ token: null, role: null, nombre: null });
-    window.location.href = "/"
+    navigate("/");
   };
 
   return (
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import "../../styles/login/login.css";
 import Logo from "../header/Logo";
+import { toast, ToastContainer  } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/AuthContext";
+
 
 const Login = ({ isLoginValue }) => {
   const [isLogin, setIsLogin] = useState(isLoginValue);
   const [errorMessage, setErrorMessage] = useState("");
+  const { auth } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   // React Hook Form para manejar los inputs y validaciones
   const {
@@ -26,89 +33,127 @@ const Login = ({ isLoginValue }) => {
       // Guardar el token en el localStorage
       localStorage.setItem("token", response.data.token);
       localStorage.setItem("role", response.data.role);
-      localStorage.setItem("userName", response.data.nombre);
+      localStorage.setItem("userName", response.data.nombre || "Usuario");
 
-      // Redirigir según el rol
-      if (response.data.role === "ADMIN") {
-        window.location.href = "/administracion";
-      } else {
-        window.location.href = "/";
+      if (response.data.message != null) {
+        toast.success(response.data.message, {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });  
+        setTimeout(() => {
+          window.location.href = response.data.role === "ADMIN" ? "/admin" : "/";
+        }, 2000);
+    
       }
+
+      if (response.data.error != null) {
+        toast.error(response.data.error, {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });  
+
+        navigate("/");
+      }
+      
+      
+      
     } catch (error) {
-      setErrorMessage(
-        error.response?.data?.message || "Error en la autenticación"
-      );
+      console.error("Error en la autenticación:", error.response?.data || error);
+  
+      toast.error(error.response?.data?.message || "Error en la autenticación", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
     }
   };
 
   return (
-    <div className="login-form-container">
-      <div className="login-logo">
-        <Logo />
-      </div>      
-      <h2>{isLogin ? "Iniciar Sesión" : "Crear Cuenta"}</h2>
+    <>
+      <ToastContainer />
+      <div className="login-form-container">
+        <div className="login-logo">
+          <Logo />
+        </div>      
+        <h2>{isLogin ? "Iniciar Sesión" : "Crear Cuenta"}</h2>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {!isLogin && (
-          <>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {!isLogin && (
+            <>
+              <div>
+                <label>Nombre</label>
+                <input 
+                  type="text" 
+                  placeholder="Ingresa tu nombre" 
+                  {...register("nombre", { required: "El nombre es obligatorio" })}
+                />
+                {errors.nombre && <p className="error">{errors.nombre.message}</p>}
+              </div>
+              <div>
+                <label>Teléfono</label>
+                <input 
+                  type="tel" 
+                  placeholder="Ingresa tu número de teléfono" 
+                  {...register("telefono", { required: "El teléfono es obligatorio" })}
+                />
+                {errors.telefono && <p className="error">{errors.telefono.message}</p>}
+              </div>
+            </>
+          )}
           <div>
-            <label>Nombre</label>
+            <label>Correo Electrónico</label>
             <input 
-              type="text" 
-              placeholder="Ingresa tu nombre" 
-              {...register("nombre", { required: "El nombre es obligatorio" })}
+              type="email" 
+              placeholder="ejemplo@correo.com" 
+              {...register("email", { 
+                required: "El email es obligatorio", 
+                pattern: { value: /\S+@\S+\.\S+/, message: "Email no válido" } 
+              })}
             />
-            {errors.name && <p className="error">{errors.name.message}</p>}
+            {errors.email && <p className="error">{errors.email.message}</p>}
           </div>
           <div>
-          <label>Telefono</label>
-          <input 
-            type="phone" 
-            placeholder="Ingresa numero telefonico" 
-            {...register("telefono", { required: "El telefono es obligatorio" })}
-          />
-          {errors.phone && <p className="error">{errors.phone.message}</p>}
-        </div>
-        </>
-        )}
-        <div>
-          <label>Correo Electrónico</label>
-          <input 
-            type="email" 
-            placeholder="ejemplo@correo.com" 
-            {...register("email", { 
-              required: "El email es obligatorio", 
-              pattern: { value: /\S+@\S+\.\S+/, message: "Email no válido" } 
-            })}
-          />
-          {errors.email && <p className="error">{errors.email.message}</p>}
-        </div>
-        <div>
-          <label>Contraseña</label>
-          <input 
-            type="password" 
-            placeholder="********" 
-            {...register("contrasenia", { 
-              required: "La contraseña es obligatoria", 
-              minLength: { value: 6, message: "Mínimo 6 caracteres" } 
-            })}
-          />
-          {errors.password && <p className="error">{errors.password.message}</p>}
-        </div>
+            <label>Contraseña</label>
+            <input 
+              type="password" 
+              placeholder="********" 
+              {...register("contrasenia", { 
+                required: "La contraseña es obligatoria", 
+                minLength: { value: 6, message: "Mínimo 6 caracteres" } 
+              })}
+            />
+            {errors.contrasenia && <p className="error">{errors.contrasenia.message}</p>}
+          </div>
 
-        {errorMessage && <p className="error-message">{errorMessage}</p>}
+          <div className="login-submit-container">
+            <button type="submit" className="login-submit">
+              {isLogin ? "Iniciar Sesión" : "Registrarse"}
+            </button>
+          </div>
+        </form>
 
-        <div className="login-submit-container">
-          <button type="submit" className="login-submit">
-            {isLogin ? "Iniciar Sesión" : "Registrarse"}
-          </button>
-        </div>
-      </form>
-
-      <p className="login-toggle" onClick={() => setIsLogin(!isLogin)}>
-        {isLogin ? "¿No tienes cuenta? Regístrate" : "¿Ya tienes cuenta? Inicia Sesión"}
-      </p>
-    </div>
+        <p className="login-toggle" onClick={() => setIsLogin(!isLogin)}>
+          {isLogin ? "¿No tienes cuenta? Regístrate" : "¿Ya tienes cuenta? Inicia Sesión"}
+        </p>
+      </div>
+    </>
   );
 };
 
