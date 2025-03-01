10c0a2577ec1a54859602faeb7953149e68dccb6
Roberto Vélez <robertojvi@gmail.com>
2025-03-01 09:13:26 -0500

    refactor: clean up imports and remove unused variables in forms

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { Link } from "react-router-dom";
 import warningIcon from "../images/warning.png";
 import "../styles/admin/admin.css";
