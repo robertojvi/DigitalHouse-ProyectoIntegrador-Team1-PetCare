fe458e9d424b64bc9500f9e5a2bcd417bd29785b
ricardocuellar <rcuellarsn@gmail.com>
2025-03-05 21:00:42 -0600

    Auto stash before merge of "development" and "origin/development"

--- src/components/GridComponent.jsx
+++ src/components/GridComponent.jsx
@@ -15,13 +15,8 @@ export const GridComponent = () => {
 			try {
 				setLoading(true);
 				const data = await getServices();
-				console.log("DATA received:", data); // Depuración
-				if (Array.isArray(data)) {
-					setProfiles(data);
-				} else {
-					console.error("Expected an array but received:", data);
-					setProfiles([]); // En caso de error, establecer un array vacío
-				}
+				console.log("DATA: ",data.listaServicios);
+				setProfiles(data);
 			} catch (error) {
 				console.error("Error loading profiles:", error);
 				setProfiles([]); // En caso de error, establecer un array vacío
