b9dfe80c4800795bff865b560435c61625e45014
luciacerpap <luciacerpap@gmail.com>
2025-03-18 10:48:04 -0600

    imagenes ver mas

--- src/pages/services/ServiceImagesGallery.jsx
+++ src/pages/services/ServiceImagesGallery.jsx
@@ -30,7 +30,7 @@ const ServiceGallery = () => {
 				<div className="gallery-grid">
 					{images.map((image, index) => (
 						<div key={index} className="gallery-item">
-							<img src={image} alt={`Pet ${index + 1}`} />
+							<img src={image?.imagenUrl || "https://via.placeholder.com/150"} alt={`Pet ${index + 1}`} />
 						</div>
 					))}
 				</div>
