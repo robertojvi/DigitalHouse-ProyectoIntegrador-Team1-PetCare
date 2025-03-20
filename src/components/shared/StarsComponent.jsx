1a9b87e88b706839c59ee089e84ad314c3aff4c4
ricardocuellar <rcuellarsn@gmail.com>
2025-03-20 15:12:47 -0600

    Add empty stars to card component

--- src/components/shared/StarsComponent.jsx
+++ src/components/shared/StarsComponent.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import Star from "../../assets/icons/Star.svg"
+import React from 'react';
+import Star from "../../assets/icons/Star.svg";
+import StarEmpty from "../../assets/icons/StarEmpty.png";
 
-export const StarsComponent = ({rating}) => {
+export const StarsComponent = ({ rating }) => {
   return (
     <div style={{ display: 'flex', gap: '0px' }}>
-        {Array.from({ length: rating }, (_, index) => (
-        <img key={index} src={Star} alt="Star" height={30} />
-      ))}
+      {rating && rating > 0
+        ? Array.from({ length: rating }, (_, index) => (
+            <img key={index} src={Star} alt="Star" height={30} />
+          ))
+        : Array.from({ length: 5 }, (_, index) => (
+          <img src={StarEmpty} alt="Star Empty" height={12} />
+        ))}
     </div>
-  )
-}
+  );
+};
