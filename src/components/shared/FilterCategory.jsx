ad4eb50877b4279e2604ea08b81fbeb19bac943d
ricardocuellar <rcuellarsn@gmail.com>
2025-03-21 18:08:48 -0600

    Add url base

--- src/components/shared/FilterCategory.jsx
+++ src/components/shared/FilterCategory.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { FilterCategoryContainer, FilterCategoryLink } from './styled-components/FilterCategory.styles'
 import { getAppUrl } from '../../services/getAppUrl'
 
+const BASE_URL = import.meta.env.VITE_API_URL || "";
+
+
 export const FilterCategory = ({ name, icon, id }) => {
 
     return (
         <FilterCategoryContainer>
-            <FilterCategoryLink href={`${getAppUrl}/categories/${id+1}`}>
+            <FilterCategoryLink href={`${BASE_URL}/categories/${id+1}`}>
             <img src={icon} alt={`${name} icon`} />
             <p>{name}</p>
             </FilterCategoryLink>
