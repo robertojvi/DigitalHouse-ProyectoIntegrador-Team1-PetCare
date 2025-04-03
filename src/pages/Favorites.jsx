b35d30272c43f046a4b825dea0ad4fc50bb36732
ricardocuellar <rcuellarsn@gmail.com>
2025-04-03 01:36:55 -0600

    Resolve dynamic favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,54 +1,19 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../auth/AuthContext";
 import { ServiceCard } from "../components/cards/ServiceCard";
 import pawprint from "../assets/icons/pawprint.svg";
-import mockFavorites from "../mocks/favoritesData.json";
-import "../styles/pages/favorites.css";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import "../styles/pages/favorites.css";
 
 const Favorites = () => {
-  const [favorites, setFavorites] = useState([]);
-  const { auth } = useContext(AuthContext);
+  const { favoritos } = useContext(AuthContext);
   const navigate = useNavigate();
-  const BASE_URL = import.meta.env.VITE_API_URL || "";
-  const API_URL_GET_USER = `${BASE_URL}/api/usuarios`;
+  const [favorites, setFavorites] = useState([]);
 
+  // Escucha cambios en favoritos del contexto
   useEffect(() => {
-    // Simulamos una llamada a la API con un pequeño retraso
-    // const fetchFavorites = async () => {
-    //   try {
-    //     // Simulamos un delay para que parezca una llamada real
-    //     await new Promise((resolve) => setTimeout(resolve, 500));
-    //     JSON.parse(localStorage.getItem("favorites"))
-    //     setFavorites(mockFavorites.favorites);
-    //   } catch (error) {
-    //     console.error("Error fetching favorites:", error);
-    //   }
-    // };
-
-    // fetchFavorites();
-
-    // Comentamos la llamada real a la API por ahora
-    if (auth.token) {
-      const fetchFromAPI = async () => {
-        
-        try {
-          const responseUsuario = await axios.get(`${API_URL_GET_USER}/${auth.idUsuario}`, {
-            headers: {
-              Authorization: `Bearer ${auth.token}`
-            }
-          });
-          
-          console.log(responseUsuario)
-          setFavorites(responseUsuario.data.favoritos);
-        } catch (error) {
-          console.error("Error fetching favorites:", error);
-        }
-      };
-      fetchFromAPI();
-    } 
-  }, []);
+    setFavorites(favoritos || []);
+  }, [favoritos]);
 
   const handleImageClick = (serviceId) => {
     navigate(`/service/${serviceId}`);
@@ -61,6 +26,7 @@ const Favorites = () => {
         <h1>Mis Favoritos</h1>
         <img src={pawprint} alt="Huella" className="pawprint" />
       </div>
+
       {favorites.length > 0 ? (
         <div className="favorites-grid">
           {favorites.map((service) => (
@@ -72,7 +38,7 @@ const Favorites = () => {
               image={service.imagenUrls[0]?.imagenUrl}
               rating={service.rating}
               excerpt={service.descripcion}
-              onImageClick={() => handleImageClick(service.idService)}
+              onImageClick={() => handleImageClick(service.idServicio)}
               isFavorito={true}
             />
           ))}
