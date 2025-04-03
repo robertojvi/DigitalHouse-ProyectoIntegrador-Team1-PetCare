404cff6c7000dc8a8df3cbf632eb3e7a61103c20
ricardocuellar <rcuellarsn@gmail.com>
2025-04-03 02:23:58 -0600

    Resolve navigate to services and context reload

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,23 +1,51 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { AuthContext } from "../auth/AuthContext";
 import { ServiceCard } from "../components/cards/ServiceCard";
 import pawprint from "../assets/icons/pawprint.svg";
-import { useNavigate } from "react-router-dom";
+import mockFavorites from "../mocks/favoritesData.json";
 import "../styles/pages/favorites.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Favorites = () => {
-  const { favoritos } = useContext(AuthContext);
-  const navigate = useNavigate();
   const [favorites, setFavorites] = useState([]);
+  const { favoritos } = useContext(AuthContext);
+  const { auth } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const BASE_URL = import.meta.env.VITE_API_URL || "";
+  const API_URL_GET_USER = `${BASE_URL}/api/usuarios`;
+  const [loading, setLoading] = useState(true);
 
-  // Escucha cambios en favoritos del contexto
   useEffect(() => {
-    setFavorites(favoritos || []);
+    if (auth.token) {
+      const fetchFromAPI = async () => {
+        
+        try {
+          const responseUsuario = await axios.get(`${API_URL_GET_USER}/${auth.idUsuario}`, {
+            headers: {
+              Authorization: `Bearer ${auth.token}`
+            }
+          });
+          
+          console.log(responseUsuario)
+          setFavorites(responseUsuario.data.favoritos);
+        } catch (error) {
+          console.error("Error fetching favorites:", error);
+        }
+      };
+      fetchFromAPI();
+      setLoading(false);
+    } 
   }, [favoritos]);
 
-  const handleImageClick = (serviceId) => {
-    navigate(`/service/${serviceId}`);
+  const handleImageClick = (service) => {
+    navigate(`/service/${service.idServicio}`, {
+      state: { selectedService: service },
+    });
+    window.scrollTo(0, 0);
   };
+
+  
 
   return (
     <div className="favorites-container">
@@ -26,26 +54,32 @@ const Favorites = () => {
         <h1>Mis Favoritos</h1>
         <img src={pawprint} alt="Huella" className="pawprint" />
       </div>
-
-      {favorites.length > 0 ? (
-        <div className="favorites-grid">
-          {favorites.map((service) => (
-            <ServiceCard
-              key={service.idServicio}
-              id={service.idServicio}
-              name={service.nombre}
-              serviceType={service.categoria.nombre}
-              image={service.imagenUrls[0]?.imagenUrl}
-              rating={service.rating}
-              excerpt={service.descripcion}
-              onImageClick={() => handleImageClick(service.idServicio)}
-              isFavorito={true}
-            />
-          ))}
-        </div>
+      { !loading ? (
+        <>
+        {favorites.length > 0 ? (
+          <div className="favorites-grid">
+            {favorites.map((service) => (
+              <ServiceCard
+                key={service.idServicio}
+                id={service.idServicio}
+                name={service.nombre}
+                serviceType={service.categoria.nombre}
+                image={service.imagenUrls[0]?.imagenUrl}
+                rating={service.rating}
+                excerpt={service.descripcion}
+                onImageClick={() => handleImageClick(service)}
+                isFavorito={true}
+              />
+            ))}
+          </div>
+        ) : (
+          <p className="no-favorites">No tienes servicios favoritos guardados.</p>
+        )}
+        </>
       ) : (
-        <p className="no-favorites">No tienes servicios favoritos guardados.</p>
+          <p className="no-favorites">Cargando...</p>
       )}
+      
     </div>
   );
 };
